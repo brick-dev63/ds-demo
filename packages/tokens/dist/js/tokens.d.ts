1d0c8f61e06ec98f9cb94b017b89c673cd0534f4
Rick Bowman <rickbowman63@gmail.com>
2022-01-03 14:32:08 -0700

    initial work

---
+++ packages/tokens/dist/js/tokens.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 03 Jan 2022 21:25:04 GMT
+ */
+
+export const ColorBaseGrayLight : string;
+export const ColorBaseGrayMedium : string;
+export const ColorBaseGrayDark : string;
+export const ColorBaseRed : string;
+export const ColorBaseGreen : string;
+export const ColorFontBase : string;
+export const ColorFontSecondary : string;
+export const ColorFontTertiary : string;
+export const SizeFontSmall : string; // the small size of the font
+export const SizeFontMedium : string; // the medium size of the font
+export const SizeFontLarge : string; // the large size of the font
+export const SizeFontBase : string; // the base size of the font
